92189593db3091cd60cb1a7b1f3d29f99c4ce9b0
Mia <miaelanderson@gmail.com>
2024-11-12 09:45:00 -0600

    Initial Commit

---
+++ Three letter word/Three_letter_wordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_letter_wordApp.swift
+//  Three letter word
+//
+//  Created by Mia Anderson on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Three_letter_wordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
