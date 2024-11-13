52f930924a5439b6f2093bc74495abfaba6ded9a
Mia <miaelanderson@gmail.com>
2024-11-13 09:51:55 -0600

    added tap gesture recognition

--- Three letter word/ContentView.swift
+++ Three letter word/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var letter = ""
+    @State private var counter = 0
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Three Letter Word")
+                .font(.title).bold()
+                .padding()
+            Text("Tap the gray box to change the letter.")
+            CustomLetterBox(color: .gray, text: letter)
+                .onTapGesture {
+                    let position = alphabet.index(alphabet.startIndex, offsetBy: counter)
+                    letter = String(alphabet[position])
+                    counter += 1
+                    if counter == alphabet.count {
+                        counter = 0
+                }
+            }
         }
-        .padding()
+    }
+}
+
+struct CustomLetterBox: View {
+    let color : Color
+    let text : String
+    var body : some View {
+        ZStack {
+            color
+            Text(text)
+                .font(.system(size: 90))
+                .fontWeight(.heavy)
+        }
+        .frame(width: 120, height: 120)
     }
 }
 
